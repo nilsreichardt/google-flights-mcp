b492b0dae7153de6f0a6039896bef3c5a5a249e6
nilsreichardt <me@nils.re>
2025-11-05 10:08:11 -0800

    Exit deploy script on error

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@ PROJECT_ID="..."
 # Need to be exported for sudo to work
 export IMAGE="gcr.io/$PROJECT_ID/google-flights-cheapest-offers:latest"
 
+set -e # Exit on error
+
 sudo docker buildx build --platform linux/amd64 -t $IMAGE --push .
 sudo docker push $IMAGE
 
